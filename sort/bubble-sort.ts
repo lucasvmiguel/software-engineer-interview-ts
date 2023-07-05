db20f0cfa656558dc7084571fdf1778b78425581
Lucas Miguel <lucasvieira.dev@gmail.com>
2023-07-05 14:25:59 +0200

    added selction sorting and updated bubble sort

--- sort/bubble-sort.ts
+++ sort/bubble-sort.ts
@@ -1,13 +1,11 @@
-export function bubbleSort(arr: number[]): number[] {
+export function bubbleSort(arr: number[]) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[i] < arr[j]) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
         const temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
-
-  return arr;
 }
